aa93edccbc34b534e0b5d4f1749f4c5269f22bb4
Andreyeyeye <shebanoff.andrei@yandex.ru>
2024-09-05 14:50:15 +0700

    Добавьте файлы проекта.

---
+++ zi_labs/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zi_labs
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        lab1 Lab1 = new lab1();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private void button_click_l11(object sender, RoutedEventArgs e)
+        {
+            ulong val;
+            try
+            {
+                UInt64.Parse(l11val.Text);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("невозможно перевести");
+                l11out.Content = "некорректные данные";
+                return;
+                //   MessageBox.Show("Неправильный идентификатор Лифтового блока");
+            }
+            val = UInt64.Parse(l11val.Text);
+            ulong p;
+            try
+            {
+                UInt64.Parse(l11p.Text);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("невозможно перевести");
+                l11out.Content = "некорректные данные";
+                return;
+                //   MessageBox.Show("Неправильный идентификатор Лифтового блока");
+            }
+            p = UInt64.Parse(l11p.Text);
+
+            l11out.Content = Lab1.fast_exp(val, p).ToString();
+
+
+        }
+    }
+}
---
+++ zi_labs/lab1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zi_labs
+{
+    internal class lab1
+    {
+        public lab1() { }
+
+        public ulong fast_exp(ulong val, ulong p)
+        {
+            if (p < 0)
+            {
+                ulong temp = (ulong)Math.Pow(val, p);
+                return temp;
+
+            }
+            if (p == 0)
+            {
+                return 1;
+            }
+            if (p % 2 == 0)
+            {
+                return fast_exp(val * val, p / 2);
+            }
+            else
+            {
+                return val*fast_exp(val*val, (p-1)/2);
+            }
+            // https://habr.com/ru/companies/otus/articles/779396/
+        }
+        public List<ulong> eucl(ulong a, ulong b) 
+        { 
+        
+            
+            return new List<ulong>(); 
+        
+        }
+    }
+}
