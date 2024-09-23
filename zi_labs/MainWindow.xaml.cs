a2802740e4c721c50a0ccf933a937bbceff51e2b
Andreyeyeye <shebanoff.andrei@yandex.ru>
2024-09-23 23:00:20 +0700

    черновая работа выполнена

--- zi_labs/MainWindow.xaml.cs
+++ zi_labs/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,30 +21,48 @@ namespace zi_labs
     /// </summary>
     public partial class MainWindow : Window
     {
-        lab1 Lab1 = new lab1();
         public MainWindow()
         {
             InitializeComponent();
-
-
-
+            Console.WriteLine("Функция быстрого возведения числа в степень по модулю:");
+            Console.WriteLine(lab1.fast_exp(4, 2, 3));
+            Console.WriteLine("обобщённый алгоритм Евклида:");
+            List<ulong> t = lab1.eucl(10, 200);
+            Console.WriteLine(t[0].ToString() + " " + t[1].ToString() + " " + t[2].ToString());
+            Console.WriteLine("Диффи-Хеллман:");
+            lab1.diffie_hellman_algorithm();
+            Console.WriteLine("Шаг младенца Шаг великана");
+            Console.WriteLine(lab1.giant_baby_step(88, 107, 47));
         }
 
         private void button_click_l11(object sender, RoutedEventArgs e)
         {
-            ulong val;
+            ulong a;
             try
             {
-                UInt64.Parse(l11val.Text);
+                UInt64.Parse(l11a.Text);
             }
             catch (FormatException)
             {
                 Console.WriteLine("невозможно перевести");
-                l11out.Content = "некорректные данные";
+                l11out.Text = "некорректные данные";
                 return;
-                //   MessageBox.Show("Неправильный идентификатор Лифтового блока");
             }
-            val = UInt64.Parse(l11val.Text);
+            a = UInt64.Parse(l11a.Text);
+            ulong x;
+            try
+            {
+                UInt64.Parse(l11x.Text);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("невозможно перевести");
+                l11out.Text = "некорректные данные";
+                return;
+
+            }
+            x = UInt64.Parse(l11x.Text);
+
             ulong p;
             try
             {
@@ -52,15 +71,108 @@ namespace zi_labs
             catch (FormatException)
             {
                 Console.WriteLine("невозможно перевести");
-                l11out.Content = "некорректные данные";
+                l11out.Text = "некорректные данные";
                 return;
-                //   MessageBox.Show("Неправильный идентификатор Лифтового блока");
+
             }
             p = UInt64.Parse(l11p.Text);
-
-            l11out.Content = Lab1.fast_exp(val, p).ToString();
+            ulong l11o = lab1.fast_exp(a, x, p);
+            l11out.Text = l11o.ToString();
 
 
         }
+
+        private void button_click_l12(object sender, RoutedEventArgs e)
+        {
+            ulong a;
+            try
+            {
+                UInt64.Parse(l12a.Text);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("невозможно перевести");
+                l12out.Text = "некорректные данные";
+                return;
+            }
+            a = UInt64.Parse(l12a.Text);
+            ulong x;
+            try
+            {
+                UInt64.Parse(l12x.Text);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("невозможно перевести");
+                l12out.Text = "некорректные данные";
+                return;
+
+            }
+            x = UInt64.Parse(l12x.Text);
+
+
+            List<ulong> l12o = lab1.eucl(a, x);
+            l12out.Text = "= " + l12o[0].ToString() + "  k1: " + l12o[1].ToString() + "  l2: " + l12o[2].ToString();
+
+
+        }
+        private void button_click_l13(object sender, RoutedEventArgs e)
+        {
+            List<ulong> L = lab1.diffie_hellman_algorithm();
+            l13out1.Text = "Закрытые ключи a: " + L[0] + "  b: " + L[1];
+            l13out2.Text = "Открытые ключи Ya: " + L[2] + "  Yb: " + L[3];
+            l13out3.Text = "Секретные ключи Zab: " + L[4] + "  Zba: " + L[5];
+
+
+        }
+
+
+        private void button_click_l14(object sender, RoutedEventArgs e)
+        {
+            ulong a;
+            try
+            {
+                UInt64.Parse(l14a.Text);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("невозможно перевести");
+                l11out.Text = "некорректные данные";
+                return;
+            }
+            a = UInt64.Parse(l14a.Text);
+            ulong x;
+            try
+            {
+                UInt64.Parse(l14x.Text);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("невозможно перевести");
+                l11out.Text = "некорректные данные";
+                return;
+
+            }
+            x = UInt64.Parse(l14x.Text);
+
+            ulong p;
+            try
+            {
+                UInt64.Parse(l14p.Text);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("невозможно перевести");
+                l11out.Text = "некорректные данные";
+                return;
+
+            }
+            p = UInt64.Parse(l14p.Text);
+            ulong l14o = (ulong)lab1.giant_baby_step(a, x, p);
+            l14out.Text = l14o.ToString();
+
+
+        }
+
     }
 }
--- zi_labs/lab1.cs
+++ zi_labs/lab1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace zi_labs
 {
@@ -10,34 +12,159 @@ namespace zi_labs
     {
         public lab1() { }
 
-        public ulong fast_exp(ulong val, ulong p)
+        public static ulong fast_exp(ulong a, ulong x, ulong p)
         {
-            if (p < 0)
+            ulong result = 1;
+            a = a % p;
+
+            if (a == 0)
             {
-                ulong temp = (ulong)Math.Pow(val, p);
-                return temp;
+                return 0;
+            }
+
+            while (x > 0)
+            {
+                if ((x & 1) == 1)  // если крайний правый бит степени равен 1
+                {
+                    result = (result * a) % p;
+                }
+
+                a = (a * a) % p;
+                x >>= 1;  // побитово смещаем степень
+            }
+
+            return result;
+        }
+        public static List<ulong> eucl(ulong a, ulong b)
+        {
+            List<ulong> U = new List<ulong> { a, 1, 0 };
+            List<ulong> V = new List<ulong> { b, 0, 1 };
+            while (V[0] != 0)
+            {
+                ulong q = U[0] / V[0];
+                List<ulong> T = new List<ulong>{ U[0] % V[0], U[1] - q * V[1], U[2] - q * V[2]};
+                U = V; 
+                V = T;
+            }
+
+            return U; 
+        }
+        public static bool check_prime(ulong p)
+        {
+            Random rnd = new Random();
+
+            if (p<=1) return false;
+            else if (p==2) return true;
+            ulong a = (ulong)rnd.Next(2, Convert.ToInt32(p) - 1);
+            if(fast_exp(a, (p-1), p) != 1 || gcd(p, a) > 1){
+                return false;
+            } 
+            return true;
+        }
+
+        public static ulong gcd(ulong a, ulong b)
+        {
+            while(b!=0)
+            {
+                ulong r = a % b;
+                a = b;
+                b = r;
+            }
+            return a;
+        }
+
+        public static ulong generate_prime(ulong left, ulong right)
+        {
+            Random rnd = new Random();
+            while (true)
+            {
+                ulong p = (ulong)rnd.Next((int)left, (int)right);
+                if(check_prime(p)) return p;
+            }
+        }
+
+        public static List<ulong> diffie_hellman_algorithm()
+        {
+            Random rnd = new Random();
+            ulong q = 0;
+            ulong p = 0;
+            ulong g = 0;
+            ulong Xa = 0;
+            ulong Xb = 0;
+            ulong Ya = 0;
+            ulong Yb = 0;
+            ulong Zab = 0;
+            ulong Zba = 0;
+            Console.WriteLine("Общие параметры: ");
+            while(true)
+            {
+                q = generate_prime(0, 1000000000);
+                p = 2 * q + 1;
+                if (check_prime(p))
+                {
+                    break;
+                } else
+                {
+                }
 
             }
-            if (p == 0)
+            Console.WriteLine("f\"q = " + q);
+            Console.WriteLine("p = " + p);
+            while (fast_exp(g, q, p) != 1)
             {
-                return 1;
+                g = (ulong)rnd.Next(1, (int)(p -1));
+
             }
-            if (p % 2 == 0)
-            {
-                return fast_exp(val * val, p / 2);
-            }
-            else
-            {
-                return val*fast_exp(val*val, (p-1)/2);
-            }
-            // https://habr.com/ru/companies/otus/articles/779396/
+            Xa = (ulong)rnd.Next(1, (int)(p));
+            Xb = (ulong)rnd.Next(1, (int)(p));
+            Console.WriteLine("Закрытые ключи: ");
+            Console.WriteLine("Xa = " + Xa);
+            Console.WriteLine("Xb = " + Xb);
+
+            Ya = fast_exp(g, Xa, p);
+            Yb = fast_exp(g, Xb, p);
+            Console.WriteLine("Открытые ключи: ");
+            Console.WriteLine("Ya = " + Ya);
+            Console.WriteLine("Yb = " + Yb);
+
+            Zab = fast_exp(Yb, Xa, p);
+            Zba = fast_exp(Ya, Xb, p);
+            Console.WriteLine("Секретные ключи: ");
+            Console.WriteLine("Zab = " + Zab);
+            Console.WriteLine("Zba = " + Zba);
+            return new List<ulong> { Xa, Xb, Ya, Yb, Zab, Zba };
         }
-        public List<ulong> eucl(ulong a, ulong b) 
-        { 
-        
-            
-            return new List<ulong>(); 
-        
+
+        public static ulong? giant_baby_step(ulong a, ulong p, ulong y)
+        {
+            ulong k = (ulong)Math.Ceiling(Math.Sqrt(p));
+            ulong m = (ulong)Math.Ceiling(Math.Sqrt(p));
+
+            Dictionary<ulong, ulong> baby = new Dictionary<ulong, ulong>();
+            for (ulong j = 0; j < m; j++)
+            {
+                ulong value = (fast_exp(a, j, p) * y) % p;
+                baby[value] = j;
+            }
+            Console.WriteLine(string.Join(", ", baby));
+
+            List<ulong> giant = new List<ulong>();
+            for (ulong i = 1; i <= k; i++)
+            {
+                giant.Add(fast_exp(a, m * i, p));
+            }
+            Console.WriteLine(string.Join(", ", giant));
+
+            for (ulong i = 1; i <= k; i++)
+            {
+                if (baby.TryGetValue(giant[(int)i - 1], out ulong j))
+                {
+                    return i * m - j;
+                }
+            }
+
+            return null;
         }
+
     }
 }
